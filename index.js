ad61548bb794e91745823226e67611bdb9f6d81a
Juan SB <juansb.dev@gmail.com>
2023-09-25 23:52:38 -0300

    builded

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
+import { registerRootComponent } from 'expo';
 
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import {MyContext} from './src/context'
-import stageOne from './src/components/stage_one';
-import stageTwo from './src/components/stage_two';
+import App from './App';
 
-
-export default class Index extends Component {
-  static contextType = MyContext;
-  render() {
-    return (
-          <View style={styles.container}>
-            <stageOne></stageOne>
-              {
-                this.context.state.stage === 1 ? (
-                    <>
-                    <stageOne></stageOne>
-                    <Text>asd</Text>
-                    </>
-                  ): (
-                    <stageTwo></stageTwo>
-                  )
-
-              }            
-            <StatusBar style="auto" />
-          </View>
-        );
-      }
-  }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blueviolet',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
